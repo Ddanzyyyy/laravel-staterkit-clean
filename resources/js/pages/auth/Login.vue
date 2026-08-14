716963be62a278d05d090fc881c1448fea54433a
Ddanzyyyy <wildansobah69@gmail.com>
2026-08-14 14:27:29 +0700

    init

--- resources/js/pages/auth/Login.vue
+++ resources/js/pages/auth/Login.vue
@@ -62,7 +62,6 @@ const submit = () => {
             <div class="mb-4 mt-6 flex items-center justify-start">
                 <Button class="w-full" :disabled="form.processing" :tabindex="4">
                     <LoaderCircle v-if="form.processing" class="h-4 w-4 animate-spin" />
-                    
                     Log in
                 </Button>
             </div>
